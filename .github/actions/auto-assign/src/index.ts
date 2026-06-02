9eda65b25ab2c46273c1229781c999e899df1842
Robert Jensen <rjensen23@gmail.com>
2026-06-02 15:53:10 -0600

    chore: update dependencies and improve logging in auto-assign action
    - Changed build script to use pnpm instead of yarn.
    - Updated @types/node from ^18.11.0 to ^20.14.13.
    - Updated typescript from ^4.9.5 to ^5.5.4.
    - Removed hardcoded engineer pool and associated logging for reviewer requests.
    - Changed console.log to console.info for assignee logging.

--- .github/actions/auto-assign/src/index.ts
+++ .github/actions/auto-assign/src/index.ts
@@ -2,8 +2,6 @@ import * as github from '@actions/github';
 import * as core from '@actions/core';
 import { PullRequestEvent } from '@octokit/webhooks-types';
 
-const engineerPool = ['evad1n', 'ckgardner', 'goodpickle', 'Thienhuynh95'];
-
 async function run() {
   try {
     const { payload, issue } = github.context;
@@ -22,17 +20,8 @@ async function run() {
     }
 
     const client = github.getOctokit(process.env.GITHUB_TOKEN).rest;
-    console.log('Trying to add reviewers', JSON.stringify(engineerPool));
-    const reviewerResult = await client.pulls.requestReviewers({
-      owner,
-      pull_number: issue_number,
-      repo,
-      /** Request all engineers that were not the author */
-      reviewers: engineerPool.filter(engineer => engineer !== author),
-    });
-    core.debug('Reviewer response: ' + JSON.stringify(reviewerResult));
 
-    console.log('Trying to add assignees: ', JSON.stringify([author]));
+    console.info('Trying to add assignees: ', JSON.stringify([author]));
 
     const result = await client.issues.addAssignees({
       owner,
